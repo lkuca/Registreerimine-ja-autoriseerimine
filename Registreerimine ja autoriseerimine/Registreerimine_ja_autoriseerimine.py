903924e9ccdd05fff210e4e42b3ac2b8965ff0a3
lkuca <120181604+lkuca@users.noreply.github.com>
2023-03-15 20:04:51 +0200

    Update Registreerimine_ja_autoriseerimine.py

--- Registreerimine ja autoriseerimine/Registreerimine_ja_autoriseerimine.py
+++ Registreerimine ja autoriseerimine/Registreerimine_ja_autoriseerimine.py
@@ -1,4 +1,27 @@
-sisselogimised=[]
-salasõnad=[]
+﻿from math import*
+from random import*
+from mymodule import*
+
+
+log=["User1", "User2"]
+parool=["s1mple", "juppi"]
+
 while True:
-    print("1-registreerimine\n2-autoriseerimine\n3-nime või parooli muutmine\n4-unustanud parooli taastamine\n5-lõpetamine")
+    print("Valige:")
+    print("1. Registreerima")
+    print("2. Logi sisse")
+    print("3. Muudata login voi salasõna")
+    print("4. Unustasid salasone")
+    print("5. Logi välja")
+    valik = input("Sisesta number (1-5): ")
+    if valik == '1':
+        registerimine()
+    elif valik == '2':
+        autoreserimine()
+    elif valik == '3':
+        muuta()
+    elif valik == '4':
+        Unustasidparooli()
+    elif valik == '5':
+        Logivälja()
+        exit()
